257794b4a9ad256a4cd2bd4a81791f23354dcddb
ThePixelatedCat <98245002+ThePixelatedCat@users.noreply.github.com>
2024-01-20 14:25:16 +1100

    cleanup

---
+++ src/main/java/frc/robot/commands/Intake/IntakeIn.java
@@ -0,0 +1,29 @@
+package frc.robot.commands.Intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.CAN;
+import frc.robot.subsystems.Intake;
+
+public class IntakeIn extends Command
+{
+    public void initialize() {
+       
+    }
+    
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+    
+     Intake.intakeIn();
+        
+    }
+    
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    Intake.intakeStop();
+    
+    }
+}
