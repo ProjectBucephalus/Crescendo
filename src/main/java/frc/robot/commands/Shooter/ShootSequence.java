1d2f11bfaed2ee05d0c5a85b6598226ab72e75dc
ThePixelatedCat <98245002+ThePixelatedCat@users.noreply.github.com>
2024-01-22 12:53:48 +1100

    Comments

--- src/main/java/frc/robot/commands/Shooter/ShootSequence.java
+++ src/main/java/frc/robot/commands/Shooter/ShootSequence.java
@@ -8,14 +8,17 @@ import frc.robot.subsystems.Intake;
 
 public class ShootSequence extends Command {
     private final Intake s_Intake;
+    // motors
     private static TalonFX mShooter = new TalonFX(Constants.mShooterID);
 
+    /* sets subsystem requirements */
     public ShootSequence(Intake s_Intake) 
     {
         this.s_Intake = s_Intake;
         addRequirements(s_Intake);
     }
 
+    /* spins up the shooter, then opens the flap and fires once it's up to speed */
     public void execute() 
     {
         double mShooterSpeedDecimal = (Math.abs(mShooter.getRotorVelocity().getValueAsDouble()) / 512); // Output absolute motor speed [0..1]
@@ -27,6 +30,7 @@ public class ShootSequence extends Command {
         }
     }
 
+    /* idles shooter, and closes flap */
     public void end(boolean interrupted) 
     {
         Intake.closeFlap();
--- src/main/java/frc/robot/commands/Shooter/ShooterRev.java
+++ src/main/java/frc/robot/commands/Shooter/ShooterRev.java
@@ -5,16 +5,19 @@ import frc.robot.subsystems.Intake;
 
 public class ShooterRev extends Command {
     private final Intake s_Intake;
+    
 
+    /* sets subsystem requirements */
     public ShooterRev(Intake s_Intake) 
     {
         this.s_Intake = s_Intake;
         addRequirements(s_Intake);
     }
 
+    /* spins up the shooter to idle speed */
     public void execute() 
     {
-        Intake.spinShooter();
+        Intake.idleShooter();
     }
 
     public void end(boolean interrupted) 
