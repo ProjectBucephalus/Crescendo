e6b9244f3fc03290be3bab2978efe9193d9c408e
ToastedMice <aidan.pawson1@icloud.com>
2024-01-25 14:47:48 +1100

    fixed compile errors

---
+++ src/main/java/frc/robot/commands/Climber/ClimberRetract.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.Climber;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Climber;
+
+public class ClimberRetract extends Command {
+
+    public Climber s_Climber;
+    public boolean isFinished;
+
+    public ClimberRetract(Climber s_Climber) {
+        this.s_Climber = s_Climber;
+        isFinished = false;
+    }
+
+    public void initialize() {
+
+    }
+
+    public void execute() {
+        s_Climber.setSpeed(1);
+        if (s_Climber.getPosition() == 0) {
+            s_Climber.setSpeed(1);
+        } else {
+            s_Climber.setSpeed(0);
+            isFinished = true;
+        }
+
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+
+    }
+}
--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,15 +2,24 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Climber extends SubsystemBase{
-    
+public class Climber extends SubsystemBase {
+
     public TalonFX mLeftClimber = new TalonFX(17);
     public TalonFX mRightClimber = new TalonFX(14);
+
+    public Climber() { 
+    }
 
     public void setSpeed(double speed) {
         mLeftClimber.set(speed);
         mRightClimber.set(-speed);
+    }
+
+    public double getPosition() {
+        SmartDashboard.putNumber("ClimberPosition", mLeftClimber.getPosition().getValueAsDouble());
+        return (mLeftClimber.getPosition().getValueAsDouble());
     }
 }
