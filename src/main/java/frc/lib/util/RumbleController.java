87318ee4524cd4d8cb9dfd4a24500bec012eec2d
Adam <51582041+SashimiDude@users.noreply.github.com>
2024-03-30 21:49:24 +1100

    auto

--- src/main/java/frc/lib/util/RumbleController.java
+++ src/main/java/frc/lib/util/RumbleController.java
@@ -1,6 +1,7 @@
 package frc.lib.util;
 
 import edu.wpi.first.math.proto.Controller;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.Command;
