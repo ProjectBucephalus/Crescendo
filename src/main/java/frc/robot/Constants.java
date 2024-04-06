3a0724d2ee06ff27ac4bf9466de85301a9291905
Driver <coach@projectb.net>
2024-04-06 10:35:07 -0700

    Updated note eject time and robot max auto speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -234,7 +234,7 @@ public final class Constants
     public static final class AutoConstants 
     { 
         /** Max drivebase speed, in meters per second */
-        public static final double kMaxSpeedMetersPerSecond = 2;
+        public static final double kMaxSpeedMetersPerSecond = 3;
         /** Max drivebase acceleration, in meters per second per second */
         public static final double kMaxAccelerationMetersPerSecondSquared = 3;
         /** Max drivebase rotational speed, in radians per second */
