3ae2e3f254b6c23354ea8f890e5ac9deacc6471b
C0mmanderC09 <125779905+SebastianAiello@users.noreply.github.com>
2024-12-07 15:21:19 +1100

    yes

---
+++ src/main/java/frc/robot/commands/Intake/Flap/CloseFlap.java
@@ -0,0 +1,38 @@
+package frc.robot.commands.Intake.Flap;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Intake.FlapPosition;
+/**@author Sebastian Aiello  */
+
+/**
+ * intake close flap command
+ * @author 5985
+ */
+public class CloseFlap extends Command {
+
+    Intake s_Intake;
+
+    public CloseFlap (Intake s_Intake) 
+    {
+        this.s_Intake = s_Intake;
+    }
+
+    public void initialize() 
+    { 
+
+    }
+    
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() 
+    {
+        s_Intake.setFlapPosition(FlapPosition.CLOSED);
+        
+    }
+    
+    public boolean isFinished()
+    {
+        return true;
+    }
+}
